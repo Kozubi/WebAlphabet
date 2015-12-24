b1f512259fac387a2b21966c1fa034a6268ee538
Kozubi <marcink00@gmail.com>
2015-12-24 11:15:33 +0100

    - first run

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import string
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    btnList = [i for i in ("A","B","C","D","E")]
+    btnList = [i for i in string.ascii_uppercase]
    # btnList = [i for i in (1,2,3,4,5)]
     return render_template("home.html", btnList = btnList)
 
